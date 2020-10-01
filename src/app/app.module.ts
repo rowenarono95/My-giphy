05de11b07a907f162f693962a5962d9c76d7c440
rowenarono95 <rowenarono@gmail.com>
2020-10-01 11:12:58 +0300

    add serch input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GifComponent } from './gif/gif.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GifComponent
+    GifComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
