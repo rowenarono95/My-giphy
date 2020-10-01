2fbcc7bc62ff83eff145ff8a80090c79fbf40b3e
rowenarono95 <rowenarono@gmail.com>
2020-10-01 10:40:37 +0300

    created component gif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifComponent } from './gif/gif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifComponent
   ],
   imports: [
     BrowserModule,
